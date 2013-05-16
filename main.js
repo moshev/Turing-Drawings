35066f1dbb23fa341293bac47e997432e3b9707b
Moshev <mo6eeeb@gmail.com>
2013-05-16 12:27:13 +0300

    Changed palette to start with black, added possibility to write 0 symbol

--- main.js
+++ main.js
@@ -132,11 +132,11 @@ console.info = console.info || function(){};
 Map of symbols (numbers) to colors
 */
 var colorMap = [
-    255,0  ,0  ,    // Initial symbol color
     0  ,0  ,0  ,    // Black
     255,255,255,    // White
     0  ,255,0  ,    // Green
     0, ,0, ,255,    // Blue
+    255,0  ,0  ,    // Red
     255,255,0  ,
     0  ,255,255,
     255,0  ,255,
